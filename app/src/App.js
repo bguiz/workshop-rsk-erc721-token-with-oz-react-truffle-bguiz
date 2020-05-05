952b2c6e0b8bd18aa0269f2cbb9bb9504bc4e1de
bguiz <bguiz@users.noreply.github.com>
2020-05-05 16:45:19 +0800

    step: 05-03: colour representation conversion utility functions

--- app/src/App.js
+++ app/src/App.js
@@ -2,3 +2,15 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import './App.css';
 import Colours from './contracts/Colours.json';
+
+function colourHexToString(hexStr) {
+  return '#' + hexStr.substring(2);
+}
+
+function colourStringToBytes(str) {
+  if (str.length !== 7 || str.charAt(0) !== '#') {
+    throw new Error('invalid colour string');
+  }
+  const hexStr = '0x' + str.substring(1);
+  return Web3.utils.hexToBytes(hexStr);
+}
